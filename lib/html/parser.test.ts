2cd37275eb96084f450a4bc1f47a87eb1f6db93b
mineejo <mineejo.dev@gmail.com>
2023-06-12 20:55:09 +0300

    feat: replace jest with ava, for more appropriate tests with ES6

---
+++ lib/html/parser.test.ts
@@ -0,0 +1,59 @@
+import { Parser } from "./parser.js";
+import { JSDOM } from "jsdom";
+import test from "ava";
+
+function parseHtml(str: string): Document | undefined {
+  return new JSDOM(str).window._document;
+}
+
+test("parser: insertStore", (t) => {
+  const parser = new Parser(`
+    <div>
+      <store name="config">
+        <value name="author">MineEjo</value>
+        <value name="version">1.0.0</value>
+      </store>
+      <div id="content">
+        <insert store="config" value="author"></insert><insert store="config" value="version"></insert>
+      </div>
+    </div>
+  `);
+
+  const document: Document | undefined = parseHtml(parser.htmlToString());
+
+  const div = document?.getElementById("content");
+  const content = "MineEjo1.0.0" as const;
+  t.is(div?.innerHTML.includes(content), true);
+});
+
+test("parser: insertContainer", (t) => {
+  const parser = new Parser(`
+    <container name="list">
+      List
+        <div>
+          Fruits
+          <ul>
+            <li>Mango</li>
+            <li>Orange</li>
+          </ul>
+        </div>
+    </container>
+    <div id="content">
+      <insert container="list"></insert>
+      <div>...</div>
+      <insert container="list"></insert>
+    </div>
+  `);
+
+  const document: Document | undefined = parseHtml(parser.htmlToString());
+
+  const div = document?.getElementById("content");
+  const firstList = div?.children[0];
+  const secondList = div?.children[2];
+
+  t.is(
+    firstList?.innerHTML.startsWith("List") ===
+      secondList?.innerHTML.startsWith("List"),
+    true
+  );
+});

0458e0819b0442f9769ef241cf100e2604cd0de6
mineejo <mineejo.dev@gmail.com>
2023-06-12 20:53:34 +0300

    feat: add parser functions and comments

--- lib/html/parser.ts
+++ lib/html/parser.ts
@@ -5,6 +5,10 @@ type Mods = {
   [name: string]: () => void;
 };
 
+/**
+ * Parser uses a web-like DOM implementation to parses
+ * HTML and implements Hipe elements, etc., modifying existing elements.
+ */
 export class Parser {
   private readonly _document: Document | undefined;
   private mods: Mods = {
@@ -12,108 +16,99 @@ export class Parser {
     insertStore: () => {
       if (!this._document) return this;
 
-      const tag = {
-        insert: "insert",
-        store: "store",
-        attrs: {
-          store: "store",
-          value: "value",
-          name: "name",
-        },
+      const data = {
+        tagInsert: "insert",
+        tagStore: "store",
+        attrStore: "store",
+        attrValue: "value",
+        attrName: "name",
       } as const;
 
-      const replaceList: [[Element | undefined, string | undefined]] = [
+      const elementsForRemoves: Element[] = [];
+      const elementsForUpdates: [[Element | undefined, string | undefined]] = [
         [undefined, undefined],
       ];
 
-      const removeList: Element[] = [];
-
-      const elements = this._document.getElementsByTagName(tag.insert);
+      const elements = this._document.getElementsByTagName(data.tagInsert);
       for (let i = 0; i < elements.length; i++) {
         const element: Element | undefined = elements[i];
-
-        const storeName = element?.getAttribute(tag.attrs.store);
-        const storeValueName = element?.getAttribute(tag.attrs.value);
-
+        const storeName = element?.getAttribute(data.attrStore);
+        const storeValueName = element?.getAttribute(data.attrValue);
         if (storeName) {
-          const store = this._document.querySelector(
-            `${tag.store}[${tag.attrs.name}=${storeName}]`
-          );
+          const storeQuery = `${data.tagStore}[${data.attrName}=${storeName}]`;
+          const store = this._document.querySelector(storeQuery);
 
           if (store) {
-            removeList.push(store);
-            const storeValues = store?.children ?? [];
+            elementsForRemoves.push(store);
+            const storeValues: HTMLCollection = store?.children ?? [];
             for (let j = 0; j <= storeValues.length - 1; j++) {
               const storeValue: Element | undefined = storeValues[j];
-
-              const valueName = storeValue?.getAttribute(tag.attrs.name);
+              const valueName = storeValue?.getAttribute(data.attrName);
               if (valueName === storeValueName && storeValue) {
-                replaceList.push([element, storeValue.innerHTML]);
+                elementsForUpdates.push([element, storeValue.innerHTML]);
               }
             }
           }
         }
       }
 
-      for (const [element, storeValue] of replaceList) {
+      for (const [element, storeValue] of elementsForUpdates) {
         if (element && storeValue) element.replaceWith(storeValue);
       }
 
-      for (const element of removeList) element.remove();
+      for (const element of elementsForRemoves) element.remove();
       return this;
     },
     // https://github.com/mineejo/hipe#container
     insertContainer: () => {
       if (!this._document) return this;
 
-      const tag = {
-        insert: "insert",
-        container: "container",
-        attrs: {
-          container: "container",
-          name: "name",
-        },
+      const data = {
+        tagInsert: "insert",
+        tagContainer: "container",
+        attrContainer: "container",
+        attrName: "name",
       } as const;
 
-      const replaceList: [[Element | undefined, Element | undefined]] = [
+      const elementsForRemoves: Element[] = [];
+      const elementsForUpdates: [[Element | undefined, Element | undefined]] = [
         [undefined, undefined],
       ];
 
-      const removeList: Element[] = [];
-
-      const elements = this._document.getElementsByTagName(tag.insert);
+      const elements = this._document.getElementsByTagName(data.tagInsert);
       for (let i = 0; i < elements.length; i++) {
         const element: Element | undefined = elements[i];
-
-        const containerName = element?.getAttribute(tag.attrs.container);
+        const containerName = element?.getAttribute(data.attrContainer);
         if (containerName) {
           const container = this._document.querySelector(
-            `${tag.container}[${tag.attrs.name}=${containerName}]`
+            `${data.tagContainer}[${data.attrName}=${containerName}]`
           );
 
           if (container) {
-            removeList.push(container);
-            replaceList.push([element, container]);
+            elementsForRemoves.push(container);
+            elementsForUpdates.push([element, container]);
           }
         }
       }
 
-      for (const [element, storeValue] of replaceList) {
+      for (const [element, storeValue] of elementsForUpdates) {
         if (element && storeValue) {
           const children: NodeListOf<ChildNode> = storeValue.childNodes;
           children.forEach(function (item: ChildNode): void {
             element?.replaceWith(element, item.cloneNode(true));
           });
-
-          removeList.push(element);
+          elementsForRemoves.push(element);
         }
       }
 
-      for (const element of removeList) element.remove();
+      for (const element of elementsForRemoves) element.remove();
       return this;
     },
   };
 
+  /**
+   * @param {string} str - HTML file, HTML strings, etc.
+   */
   constructor(str: string) {
     this._document = new JSDOM(str).window._document;
 
@@ -125,6 +120,9 @@ export class Parser {
     });
   }
 
+  /**
+   * @returns {string} Returns the finished HTML document as a string.
+   */
   htmlToString(): string {
     const html: string | undefined =
       this._document?.documentElement.outerHTML.toString();
